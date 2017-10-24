215ccffca632bd1c56fb1ba5b3e43d52040a8fe0
E. Dalton <dalton_github@gaeacoop.org>
2017-10-24 19:17:11 -0400

    anonymise -> pseudonymise

--- index.php
+++ index.php
@@ -15,26 +15,26 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * Anonymise personal identifiers
+ * Pseudonymise personal identifiers
  *
- * @package    local_anonymise
+ * @package    local_pseudonymise
  * @copyright  2016 Gavin Henrick
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 require_once(__DIR__ . '/../../config.php');
 require_once($CFG->libdir . '/adminlib.php');
-require_once($CFG->dirroot . '/local/anonymise/locallib.php');
+require_once($CFG->dirroot . '/local/pseudonymise/locallib.php');
 
-$anonymise = optional_param('action',  false,  PARAM_BOOL);
+$pseudonymise = optional_param('action',  false,  PARAM_BOOL);
 
 // Allow more time for long query runs.
 set_time_limit(0);
 
 // Start page output.
-admin_externalpage_setup('local_anonymise');
-$PAGE->set_url($CFG->wwwroot . '/local/anonymise/index.php');
-$title = get_string('pluginname', 'local_anonymise');
+admin_externalpage_setup('local_pseudonymise');
+$PAGE->set_url($CFG->wwwroot . '/local/pseudonymise/index.php');
+$title = get_string('pluginname', 'local_pseudonymise');
 $PAGE->set_title($title);
 $PAGE->set_heading($title);
 
@@ -49,11 +49,11 @@ if (!debugging() || empty($CFG->maintenance_enabled)) {
     die();
 }
 
-if ($anonymise) {
+if ($pseudonymise) {
 
     require_sesskey();
 
-    // Exectute anonmisation based on form selections.
+    // Exectute pseudonymisation based on form selections.
     $activities = optional_param('activities',  false,  PARAM_BOOL);
     $categories = optional_param('categories',  false,  PARAM_BOOL);
     $courses = optional_param('courses',  false,  PARAM_BOOL);
@@ -65,49 +65,49 @@ if ($anonymise) {
     $others = optional_param('others', false, PARAM_BOOL);
 
     if ($activities) {
-        echo $OUTPUT->heading(get_string('activities', 'local_anonymise'), 3);
-        anonymise_activities();
+        echo $OUTPUT->heading(get_string('activities', 'local_pseudonymise'), 3);
+        pseudonymise_activities();
     }
 
     if ($categories) {
-        echo $OUTPUT->heading(get_string('categories', 'local_anonymise'), 3);
-        anonymise_categories();
+        echo $OUTPUT->heading(get_string('categories', 'local_pseudonymise'), 3);
+        pseudonymise_categories();
     }
 
     if ($courses) {
-        echo $OUTPUT->heading(get_string('courses', 'local_anonymise'), 3);
-        anonymise_courses($site);
+        echo $OUTPUT->heading(get_string('courses', 'local_pseudonymise'), 3);
+        pseudonymise_courses($site);
     }
 
     if ($files) {
-        echo $OUTPUT->heading(get_string('files', 'local_anonymise'), 3);
-        anonymise_files();
+        echo $OUTPUT->heading(get_string('files', 'local_pseudonymise'), 3);
+        pseudonymise_files();
     }
 
     if ($users) {
-        echo $OUTPUT->heading(get_string('users', 'local_anonymise'), 3);
-        anonymise_users($password, $admin);
+        echo $OUTPUT->heading(get_string('users', 'local_pseudonymise'), 3);
+        pseudonymise_users($password, $admin);
     }
 
     if ($others) {
-        echo $OUTPUT->heading(get_string('others', 'local_anonymise'), 3);
-        anonymise_others($activities, $password);
+        echo $OUTPUT->heading(get_string('others', 'local_pseudonymise'), 3);
+        pseudonymise_others($activities, $password);
     }
 
-    echo html_writer::tag('p', get_string('done', 'local_anonymise'), array('style' => 'margin-top: 20px;'));
-    $purgeprompt = get_string('purgeprompt', 'local_anonymise');
+    echo html_writer::tag('p', get_string('done', 'local_pseudonymise'), array('style' => 'margin-top: 20px;'));
+    $purgeprompt = get_string('purgeprompt', 'local_pseudonymise');
     $purgeprompt .= ' ';
     $params = array('sesskey' => sesskey(), 'confirm' => '1', 'returnurl' => '/');
     $url = new moodle_url('/admin/purgecaches.php', $params);
-    $purgeprompt .= html_writer::link($url, get_string('purgelink', 'local_anonymise'));
+    $purgeprompt .= html_writer::link($url, get_string('purgelink', 'local_pseudonymise'));
     $purgeprompt .= '.';
     echo html_writer::tag('p', $purgeprompt, array('style' => 'margin-top: 20px;'));
 
 } else {
 
     // Display the form.
-    echo $OUTPUT->notification(get_string('warning', 'local_anonymise'));
-    $mform = new local_anonymise_form(new moodle_url('/local/anonymise/'));
+    echo $OUTPUT->notification(get_string('warning', 'local_pseudonymise'));
+    $mform = new local_pseudonymise_form(new moodle_url('/local/pseudonymise/'));
     $mform->display();
 
 }
