e68bf819db05cbab36bfc33d8216f435fc6a666d
E. Dalton <dalton_github@gaeacoop.org>
2017-10-28 16:28:35 -0400

    Update locallib.php

--- locallib.php
+++ locallib.php
@@ -822,7 +822,8 @@ function assign_serial_pseudo_id($len) {
     $verblist = explode(",", "Activating,Blending,Creating,Developing,Educating,Forming,Grouping,Honoring,Instantiating,Joining,Kindling,Lassoing,Moderating,Naming,Ordering,Pacifying,Quieting,Renewing,Sampling,Teaching,Understanding,Valuing,Winning,Xenografting,Yoking,Zeroing");
     $adverblist = explode(",", "Absolutely,Brilliantly,Charismatically,Deeply,Excellently,Fabulously,Graphically,Honestly,Intently,Justly,Keenly,Lively,Mostly,Nearly,Oddly,Perfectly,Quaintly,Really,Sharply,Truly,Utterly,Very,Wholly,Xtremely,Yearly,Zealously");
     $vegetablelist = explode(",", "Artichoke,Beets,Celery,Daikon,Eggplant,Fennel,Garlic,Horseradish,Ivy,Jícama,Kale,Lettuce,Mustard,Napa,Okra,Parsnip,Quandong,Radicchio,Shallots,Turnips,Ulluco,Vegetable,Watercress,Xocolatl,Yam,Ziti");
-       debugging('count serial pseudoids ' . $countserialpseudoids, DEBUG_DEVELOPER);
+
+	debugging('count serial pseudoids ' . $countserialpseudoids, DEBUG_DEVELOPER);
 
      do {
     $maxcount = 1;
@@ -835,6 +836,7 @@ $fruitcount = count($fruitlist);
 	     //print "debug maxcount $maxcount\n";
 	    // print "debug maxcount $maxcount\n";
      $id = $fruitlist[fmod($countserialpseudoids,$maxcount)];
+	debugging('fmod ' . $countserialpseudoids . ' ' . fmod($countserialpseudoids,$maxcount), DEBUG_DEVELOPER);
      
      if ($len > $maxcount) {
     // 2 words: animal with fruit 26*26 = 676
