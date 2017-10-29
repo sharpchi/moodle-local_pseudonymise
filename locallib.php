7f8661253ca1e31b062466533e11ab77d7a22e7e
E. Dalton <dalton_github@gaeacoop.org>
2017-10-28 20:21:40 -0400

    Update locallib.php

--- locallib.php
+++ locallib.php
@@ -841,7 +841,7 @@ function assign_serial_pseudo_id($len) {
     $adverblist = explode(",", "Absolutely,Brilliantly,Charismatically,Deeply,Excellently,Fabulously,Graphically,Honestly,Intently,Justly,Keenly,Lively,Mostly,Nearly,Oddly,Perfectly,Quaintly,Really,Sharply,Truly,Utterly,Very,Wholly,Xtremely,Yearly,Zealously");
     $vegetablelist = explode(",", "Artichoke,Beets,Celery,Daikon,Eggplant,Fennel,Garlic,Horseradish,Ivy,Jícama,Kale,Lettuce,Mustard,Napa,Okra,Parsnip,Quandong,Radicchio,Shallots,Turnips,Ulluco,Vegetable,Watercress,Xocolatl,Yam,Ziti");
 
-	//debugging('count serial pseudoids ' . $countserialpseudoids, DEBUG_DEVELOPER);
+	debugging('count serial pseudoids ' . $countserialpseudoids, DEBUG_DEVELOPER);
 
      do {
     $maxcount = 1;
